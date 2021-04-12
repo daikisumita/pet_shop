cc05d262a11c3a07cc2d27df90fd296dac465ab1
daikisumita <daikisumita@gmail.com>
2021-04-12 15:01:59 +0900

    fourth commit

--- index.php
+++ index.php
@@ -22,17 +22,17 @@ $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <title>pet-shop</title>
 </head>
 <body>
-    <h2 class="yohaku">本日のご紹介ペット!</h2>
-    <ul>
+    <h2 class="title">本日のご紹介ペット!</h2>
+    <ul class="pet-lis">
         <?php foreach ($animals as $animal): ?>
-            <li class="style">
+            <li>
                 <?= h($animal['type']) . 'の' . h($animal['classification']) . 'ちゃん' .
                     '<br>' . h($animal['description'] ) . '<br>' . 
                     h($animal['birthday']) . ' ' . '生まれ'. '<br> ' .
                     '出身地' . ' ' . h($animal['birthplace'])?>
             </li>
-        <hr>
-    <?php endforeach; ?>
+            <hr>
+        <?php endforeach; ?>
     </ul>
 </body>
 </html>
